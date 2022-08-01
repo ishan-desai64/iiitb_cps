2781348a805b171db5a0de40cf7bfedd72a0aea7
Ishan Desai <70513539+ishan-desai64@users.noreply.github.com>
2022-08-01 15:40:40 +0530

    Update yosys_run.sh

--- yosys_run.sh
+++ yosys_run.sh
@@ -1,14 +1,14 @@
 # read design
 
-read_verilog 32bit_carry_ripple_adder.v
+read_verilog iiitb_cps.v
 
 # generic synthesis
-synth -top ripple_carry_adder
+synth -top iiitb_cps
 
 # mapping to mycells.lib
-dfflibmap -liberty /usr/local/share/qflow/tech/osu018/osu018_stdcells.lib
-abc -liberty /usr/local/share/qflow/tech/osu018/osu018_stdcells.lib
+dfflibmap -liberty /home/ishan/iiitb_cps/lib/sky130_fd_sc_hd__tt_025C_1v80.lib
+abc -liberty /home/ishan/iiitb_cps/lib/sky130_fd_sc_hd__tt_025C_1v80.lib
 clean
 flatten
 # write synthesized design
-write_verilog -assert synth_ripple_carry_adder.v
+write_verilog -noattr iiitb_cps_synth.v
