f37776f606c6689eaf77bcedc30e4891fb1c381e
Ishan Desai <70513539+ishan-desai64@users.noreply.github.com>
2022-08-04 14:58:48 +0530

    Update yosys_run.sh

--- yosys_run.sh
+++ yosys_run.sh
@@ -7,7 +7,7 @@ synth -top iiitb_cps
 
 # mapping to mycells.lib
 dfflibmap -liberty /home/ishan/iiitb_cps/lib/sky130_fd_sc_hd__tt_025C_1v80.lib
-abc -liberty /home/ishan/iiitb_cps/lib/sky130_fd_sc_hd__tt_025C_1v80.lib
+abc -liberty /home/ishan/iiitb_cps/lib/sky130_fd_sc_hd__tt_025C_1v80.lib -script +strash;scorr;ifraig;retime,{D};strash;dch,-f;map,-M,1,{D}
 clean
 flatten
 # write synthesized design
